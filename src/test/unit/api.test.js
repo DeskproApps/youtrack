f08bb803d424839315232511b57f9362c3475425
Antonio Greco <agreco13@gmail.com>
2018-01-15 13:03:04 +0000

    feature/DP-753: Delete issue API
    Add del fetch method to utils so that a client can delete an issue
    Add deleteIssue to Youtrack api and accompanying tests/fixtures
    Rename API methods youtrackFetchIssues and youtrackCreateIssue to fetchIssues and createIssue
    Amend response messages in tests to reflect correct context

--- src/test/unit/api.test.js
+++ src/test/unit/api.test.js
@@ -1,11 +1,11 @@
 
-import { youtrackFetchIssues, youtrackCreateIssue } from '../../main/javascript/api';
+import { fetchIssues, createIssue, deleteIssue } from '../../main/javascript/api';
 import { fixtures } from './api.fixtures.js'
 
 test('successfully retrieve issues', done => {
   fetch.mockResponse(JSON.stringify(fixtures));
 
-  return youtrackFetchIssues().then(resp => {
+  return fetchIssues().then(resp => {
       expect(resp).toEqual(fixtures);
 
       fetch.resetMocks();
@@ -17,7 +17,7 @@ test('unsuccessfully retrieve issues', done => {
   const error = { message: "There was an error" };
   fetch.mockReject(error);
 
-  return youtrackFetchIssues()
+  return fetchIssues()
     .then(resp => { done(); })
     .catch(err => {
       expect(err).toEqual(error);
@@ -36,7 +36,7 @@ test('successfully create an issue with a fully payload', done => {
   };
   fetch.mockResponse(JSON.stringify({ message: success.message, payload }));
 
-  return youtrackCreateIssue(payload)
+  return createIssue(payload)
     .then(resp => {
       expect(resp.message).toEqual(success.message);
       expect(resp.payload).toEqual(payload);
@@ -48,9 +48,9 @@ test('successfully create an issue with a fully payload', done => {
 });
 
 test('unsuccessfully create an issue without a payload', done => {
-  const error = { message: 'Missing or misspelt parameters: project, summary, desc' };
+  const error = { message: 'Missing parameters: project, summary, desc' };
 
-  return youtrackCreateIssue()
+  return createIssue()
     .then(resp => { done(); })
     .catch(err => {
       err = JSON.parse(err);
@@ -69,7 +69,53 @@ test('unsuccessfully create an issue', done => {
   };
   fetch.mockReject(JSON.stringify({ message: error.message, payload }));
 
-  return youtrackCreateIssue(payload)
+  return createIssue(payload)
+    .then(resp => { done(); })
+    .catch(err => {
+      err = JSON.parse(err);
+      expect(err.message).toEqual(error.message);
+      expect(err.payload).toEqual(payload);
+
+      fetch.resetMocks();
+      done();
+    });
+});
+
+test('successfully delete an issue with a fully payload', done => {
+  const success = { message: 'An issue was successfully deleted' };
+  const payload = 'TST-123';
+  fetch.mockResponse(JSON.stringify({ message: success.message, payload }));
+
+  return deleteIssue(payload)
+    .then(resp => {
+      expect(resp.message).toEqual(success.message);
+      expect(resp.payload).toEqual(payload);
+
+      fetch.resetMocks();
+      done();
+    })
+    .catch(err => { done(); });
+});
+
+test('unsuccessfully delete an issue without a payload', done => {
+  const error = { message: 'Missing parameter: issue' };
+
+  return deleteIssue()
+    .then(resp => { done(); })
+    .catch(err => {
+      err = JSON.parse(err);
+      expect(err.message).toEqual(error.message);
+
+      done();
+    });
+});
+
+test('unsuccessfully delete an issue', done => {
+  const error = { message: 'There was an error' };
+  const payload = 'TST-123';
+  fetch.mockReject(JSON.stringify({ message: error.message, payload }));
+
+  return deleteIssue(payload)
     .then(resp => { done(); })
     .catch(err => {
       err = JSON.parse(err);
