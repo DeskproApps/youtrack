1ca0ae32701ee5bfe42d09fa9bb25ca8541de6b2
Antonio Greco <agreco13@gmail.com>
2018-01-19 12:55:35 +0000

    feature/DP-845: Settings UI
    Route user to settings page if any of the customFields are not defined

--- src/main/javascript/ui/PageSettings.jsx
+++ src/main/javascript/ui/PageSettings.jsx
@@ -12,24 +12,25 @@ class PageSettings extends React.PureComponent {
    * @returns {XML}
    */
   render() {
+    const url = 'https://www.jetbrains.com/help/youtrack/standalone/2017.2/OAuth-Authorization.html';
+
     return (
-      <Container className="dp-github-container">
+      <Container className="dp-youtrack-container">
         <h3>Setup required</h3>
         <p>
           To use the Youtrack app you must first provide the following information during installation:
-          <ul>
-            <li>
-              Youtrack Client ID: an ID of the YouTrack service that is registered in the built-in Hub service.
-            </li>
-            <li>
-               Youtrack HUB url: a url configured for the Hub service, e.g. <em>https://deskpro.myjetbrains.com</em>
-            </li>
-          </ul>
         </p>
+        <ul>
+          <li>
+            <strong>Youtrack Client ID</strong>: ID of YouTrack service registered in the built-in Hub service.
+          </li>
+          <li>
+            <strong>Youtrack HUB url</strong>: URL configured for the Hub service, e.g.
+            <em>https://deskpro.myjetbrains.com</em>
+          </li>
+        </ul>
         <p>
-          <a href="https://www.jetbrains.com/help/youtrack/standalone/2017.2/OAuth-Authorization.html">
-            Further information.
-          </a>
+          To learn more, consult the <a href={url} target="_blank" rel="noopener noreferrer">Youtrack documentation.</a>
         </p>
       </Container>
     );
