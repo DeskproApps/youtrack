2f62e73ad20b4c66bac0e815baf80a66e77c3a96
ilia makarov <ilia.makarov@me.com>
2023-01-18 23:44:32 +0200

    App setup

---
+++ src/components/Error/ErrorFallback.tsx
@@ -0,0 +1,32 @@
+import { FallbackProps } from "react-error-boundary";
+import { Stack } from "@deskpro/app-sdk";
+import { ErrorBlock } from "./ErrorBlock";
+import { Container } from "../common";
+import type { FC } from "react";
+
+type Props = Omit<FallbackProps, "error"> & {
+    error: Error,
+};
+
+const ErrorFallback: FC<Props> = ({ error }) => {
+    const message = "There was an error!";
+    const button = null;
+
+    // eslint-disable-next-line no-console
+    console.error(error);
+
+    return (
+        <Container>
+            <ErrorBlock
+                text={(
+                    <Stack gap={6} vertical style={{ padding: "8px" }}>
+                        {message}
+                        {button}
+                    </Stack>
+                )}
+            />
+        </Container>
+    );
+};
+
+export { ErrorFallback };
