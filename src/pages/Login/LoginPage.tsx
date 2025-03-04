9c42bd247321659ce694c79d2ee119db6c7e65a0
OJ Abba <phavorabba@gmail.com>
2025-03-04 15:14:30 +0000

    [Feat] Create the "login" page

---
+++ src/pages/Login/LoginPage.tsx
@@ -0,0 +1,36 @@
+import { AnchorButton, H3, Stack } from "@deskpro/deskpro-ui"
+import { ErrorBlock } from "@/components"
+import { FC } from "react"
+import { useDeskproElements, useInitialisedDeskproAppClient } from "@deskpro/app-sdk"
+import useLogin from "./useLogin"
+
+const LoginPage: FC = () => {
+    useDeskproElements(({ registerElement, clearElements }) => {
+        clearElements()
+        registerElement("refresh", { type: "refresh_button" })
+    })
+
+    useInitialisedDeskproAppClient((client) => {
+        client.setTitle("Login")
+    }, [])
+
+    const { onSignIn, authUrl, isLoading, error } = useLogin();
+
+    return (
+        <Stack padding={12} vertical gap={12} role="alert">
+            <H3>Log into your YouTrack account.</H3>
+            <AnchorButton
+                disabled={!authUrl || isLoading}
+                href={authUrl || "#"}
+                loading={isLoading}
+                onClick={onSignIn}
+                target={"_blank"}
+                text={"Log In"}
+            />
+
+            {error && <ErrorBlock text={error} />}
+        </Stack>
+    )
+}
+
+export { LoginPage }
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,6 +5,7 @@ export { EditIssuePage } from "./EditIssuePage";
 export { HomePage } from "./HomePage";
 export { LinkPage } from "./LinkPage";
 export { LoadingPage } from "./Loading";
+export { LoginPage } from "./Login";
 export { Main } from "./Main";
 export { VerifySettings } from "./VerifySettings";
 export { ViewIssuePage } from "./ViewIssuePage";
